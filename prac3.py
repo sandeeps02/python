5411f08627929c59d83b03102f80ab45278ed07f
sandeeps02 <iamsolanki02@gmail.com>
2022-07-18 10:48:36 +0530

    practise

---
+++ prac3.py
@@ -0,0 +1,17 @@
+# x = 'name = "Virat \tKohli"\nprint(name)'
+# exec(x)
+txt = "Flipkart\tBigbillion\tDays\tShop now "
+l=txt.split(" ")
+m=len(l)
+print(m)
+print()
+n=txt.count(" ")
+print(n)
+print()
+print(pow(m,n,5))
+print()
+txt = "Flipkart\tBigbillion\tDays\tShop now "
+l=txt.split(" ")
+m=len(l)
+n=txt.count(" ")
+print(pow(m,n,5))       
---
+++ prac4.py
@@ -0,0 +1,4 @@
+import math
+x = abs(3+5j)
+print(x)
+print(math.ceil(x))
