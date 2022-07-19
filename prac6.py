4515ac51b03f5b9f645e4fafb8bd3e6dc798b0d9
sandeeps02 <iamsolanki02@gmail.com>
2022-07-19 12:27:03 +0530

    practise

---
+++ prac6.py
@@ -0,0 +1,8 @@
+# a = "a\\b\'c\\d\ne\\f"
+# x=(a.split("\\"))
+# mi=1
+# for i in x:
+#     print(len(i))
+l=["Wipro","\n","\\","Infosys","\\","Cognizant"]
+j="".join(l)
+print(j)
---
+++ prac7.py
@@ -0,0 +1,19 @@
+# list1=[1,2,3,4,5,6,7]
+# for number, i in enumerate(list1) :
+#     if i==2:
+#         print(True)
+#     else:
+#         print("{0}.{1}".format(number+1,i))
+from logging.config import valid_ident
+from subprocess import list2cmdline
+
+
+list2=["butter",
+        "bread"]
+valid=[]
+# valid=[str(i) for i in range(1,len(list2)+1)]
+for i in range(1,len(list2)+1):
+    print(valid.append(str(i)))
+else:
+    print("!!!!!")
+     
