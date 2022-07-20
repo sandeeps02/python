b96b62d55618aa5a0f6db538091589f6c9933360
sandeeps02 <iamsolanki02@gmail.com>
2022-07-20 16:20:19 +0530

    practise

---
+++ pract12.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+def solve(s):
+    for i in s:
+        s=s.split(" ")
+        s=s.replace(i,i.capitalize())
+    return s
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
+    
