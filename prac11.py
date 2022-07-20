a5438a3bb41acb2fbe6c8b769f682b1d367e98e1
sandeeps02 <iamsolanki02@gmail.com>
2022-07-20 15:47:02 +0530

    practise

--- prac11.py
+++ prac11.py
@@ -3,7 +3,9 @@ matrix = [[0, 1, 2],
           [6, 7, 8],
           [9, 10, 11]]
 for i in matrix:
-    for j in i:
-        if j != 7:
-            print (j)
-    print()
+    values=", ".join(values for values in i  )
+    print=(i[values])
+    # for j in i:
+    #     if j != 7:
+    #         print (j)
+    # print()
