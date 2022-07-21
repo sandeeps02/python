b5d7dbfa23e4bc031b092c948bdf961cb8bdeb8a
sandeeps02 <iamsolanki02@gmail.com>
2022-07-21 13:52:02 +0530

    practise

--- prac15.py
+++ prac15.py
@@ -1,9 +1,10 @@
-
-# list of items
-from tkinter.messagebox import YES
-
-
-list = [1,]
- 
-# Will print the index of 'cat' in list2
-
+import math
+L = [3, 1, 2, 4]
+T = ('A', 'b', 'c', 'd')
+L.sort()
+counter = 0
+for x in T:
+    L[counter] += ord(x)
+    counter += 1
+    break
+print(math.ceil(max(L)/min(L)))
